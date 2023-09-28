0ae9243b50373d4a11d92d3440543c56d2cfea94
Ogata Mikihisa <ogata.mikihisa.777@gmail.com>
2023-09-28 18:28:10 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# フィボナッチ数の計算
+def fib_calc(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        a, b = 0, 1
+        for _ in range(n - 1):
+            a, b = b, a + b
+        return b
+
+# フィボナッチ数を返すAPIエンドポイント
+@app.route('/sample.com/fib', methods=['GET'])
+def get_fib():
+    try:
+        n = int(request.args.get('n'))
+        if n < 0:
+            return jsonify({'error': 'negative in not supported'}), 400
+        result = fib_calc(n)
+        return jsonify({'n': n, 'result': result})
+    except ValueError:
+        return jsonify({'error': 'invalid parameter'}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,34 @@
+import unittest
+from app import app
+
+class FibAPITest(unittest.TestCase):
+
+    # テストモード
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+
+    # nが自然数の時
+    def test_positive_fib(self):
+        response = self.app.get('/sample.com/fib?n=100')
+        data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['n'], 100)
+        self.assertEqual(data['result'], 354224848179261915075)
+
+    # nが負の数の時
+    def test_negative_input(self):
+        response = self.app.get('/sample.com/fib?n=-5')
+        data = response.get_json()
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['error'], 'negative in not supported')
+
+    # nが整数以外の時
+    def test_invalid_input(self):
+        response = self.app.get('/sample.com/fib?n=abc')
+        data = response.get_json()
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['error'], 'invalid parameter')
+
+if __name__ == '__main__':
+    unittest.main()
