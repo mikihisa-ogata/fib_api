98392d1dd870d3e1ad64a3785cbf18dd17ee249f
Ogata Mikihisa <ogata.mikihisa.777@gmail.com>
2023-09-30 02:33:00 +0900

    modify test.py and modify error_handling of app.py

--- app.py
+++ app.py
@@ -14,21 +14,25 @@ def fib_calc(n):
             a, b = b, a + b
         return b
 
-@app.route('/')
-def hello():
-    return "enter number"
-
 # フィボナッチ数を返すAPIエンドポイント
 @app.route('/fib', methods=['GET'])
 def get_fib():
     try:
         n = int(request.args.get('n'))
-        if n < 0:
-            return jsonify({'error': 'negative in not supported'}), 400
-        result = fib_calc(n)
-        return jsonify({'n': n, 'result': result})
-    except ValueError:
+    except Exception:
         return jsonify({'error': 'invalid parameter'}), 400
 
+    if n is None:
+        return jsonify({'error': 'please enter natural number'}), 400
+    
+    if n <= 0:
+        return jsonify({'error': 'negative in not supported'}), 400
+    
+    if n > 20000:
+        return jsonify({'error': 'n is too big'}), 400
+    
+    result = fib_calc(n)
+    return jsonify({'result': result}), 200
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- test.py
+++ test.py
@@ -1,34 +1,38 @@
 import unittest
 from app import app
+from app import fib_calc
 
-class FibAPITest(unittest.TestCase):
+# エンドポイントのテスト
+class EndpointTest(unittest.TestCase):
 
     # テストモード
     def setUp(self):
         app.config['TESTING'] = True
         self.app = app.test_client()
 
-    # nが自然数の時
-    def test_positive_fib(self):
-        response = self.app.get('/fib?n=100')
-        data = response.get_json()
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(data['n'], 100)
-        self.assertEqual(data['result'], 354224848179261915075)
+    def test_endpoint(self):
+        test_cases = [
+            (10, 200, {'result': 55}),
+            (-5, 400, {'error': 'negative in not supported'}),
+            ('abc', 400, {'error': 'invalid parameter'}),
+            (1000000, 400, {'error': 'n is too big'}),
+        ]
 
-    # nが負の数の時
-    def test_negative_input(self):
-        response = self.app.get('/fib?n=-5')
-        data = response.get_json()
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(data['error'], 'negative in not supported')
+        for n, expected_status, expected_data in test_cases:
+            with self.subTest(n=n):
+                response = self.app.get(f'/fib?n={n}')
+                data = response.get_json()
+                self.assertEqual(response.status_code, expected_status)
+                self.assertEqual(data, expected_data)
 
-    # nが整数以外の時
-    def test_invalid_input(self):
-        response = self.app.get('/fib?n=abc')
-        data = response.get_json()
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(data['error'], 'invalid parameter')
+# フィボナッチ数を計算する関数のテスト
+class FibCalcTest(unittest.TestCase):
+    def test_fib(self):
+        test_cases = [(0, 0), (1, 1), (2, 1), (3, 2), (10, 55), (-1, 0), (-5, 0)]
+        for n, expected in test_cases:
+            with self.subTest(n=n, expected=expected):
+                result = fib_calc(n)
+                self.assertEqual(result, expected)
 
 if __name__ == '__main__':
     unittest.main()
